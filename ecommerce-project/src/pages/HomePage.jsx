9abc06bb321eb0a41fc26627c98487f615ffcb7c
pavani <pavani.ku173@gmail.com>
2025-11-01 19:43:14 -0400

    fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,13 @@ import { Header } from '../components/Header';
 import { products} from '../../starting code ecommerce/data/products';
 
 export function HomePage(){
+
+  fetch('http://localhost:3000/api/products').then((response)=>{
+     //console.log(response);
+     response.json().then((data)=>{
+        console.log(data);
+     });
+  });
     return(
         <>
         <title>HomePage</title>

909b328cd9baadee1a3b2c4cb8f6fed8228cc72f
pavani <pavani.ku173@gmail.com>
2025-11-01 20:59:47 -0400

    get cart from backend and display quantity

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -6,20 +6,25 @@ import { Header } from '../components/Header';
 
 export function HomePage(){
   const[products,setProducts]=useState([]);
+  const[cart,setCart]=useState([]);
 useEffect(()=>{
-  axios.get('http://localhost:3000/api/products')
-  .then((response)=>{
-  //console.log(response);
- 
+    axios.get('http://localhost:3000/api/products')
+     .then((response)=>{
+     //console.log(response);
      setProducts(response.data);
   
-});
+   });
+      axios.get('http://localhost:3000/api/cart-items')
+      .then((response)=>{
+      setCart(response.data);
+      });
+
 },[]);
   
     return(
         <>
         <title>HomePage</title>
-      <Header />
+      <Header  cart={cart}/>
 
     <div className="home-page">
       <div className="products-grid">

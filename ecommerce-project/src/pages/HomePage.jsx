0d93320f0894b00691348fc9427ce59766caf33e
pavani <pavani.ku173@gmail.com>
2025-11-02 16:33:14 -0500

    generate html for cart items

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import { useEffect,useState } from 'react';
 import './HomePage.css';
 import { Header } from '../components/Header';
 //import { products} from '../../starting code ecommerce/data/products';
+import { formatMoney } from '../utils/money';
 
 export function HomePage({cart}){
   const[products,setProducts]=useState([]);
@@ -46,7 +47,7 @@ useEffect(()=>{
                 </div>
       
                 <div className="product-price">
-                  {(product.priceCents/100).toFixed(2)}
+                  {formatMoney(product.priceCents)}
                 </div>
       
                 <div className="product-quantity-container">
